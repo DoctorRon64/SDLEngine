7c09b523988f5381649d9f10669163ddb7c6c275
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-12-26 16:45:13 +0100

    [ADDED] Custom button renderer with label

--- src/Objects/Button.h
+++ src/Objects/Button.h
@@ -1,12 +1,17 @@
 #pragma once
 #include "Image.h"
+#include "Text.h"
+#include "../Renderers/ButtonRenderer.h"
 
 class Button : public Image {
 public:
 	using OnClick = std::function<void()>;
 
-	Button(OnClick _onClick)
-		: Image("res/btn.png", { 0, 0 }, { 3448.f, 1369.f }), onClick(_onClick) {
+	Button(OnClick _onClick, Text* _label = new Text(" "))
+		: Image("res/btn.png", { 0, 0 }, { 3448.f, 1369.f }), onClick(_onClick), label(_label) {
+
+		renderer = new ButtonRenderer(transform, "res/btn.png", { 0, 0 }, { 3448.f, 1369.f });
+
 		transform->scale = Vector2(.1f, .1f);
 
 		collider = new AABB({ 0.f, 0.f }, transform->GetSize());
@@ -18,6 +23,9 @@ public:
 	}
 
 	void Update() override {
+
+		label->GetTransform()->position = transform->position + transform->GetSize() / 4;
+
 		Vector2 mousePos = {
 			(float)InputManager::GetInstance()->GetMouseX(),
 			(float)InputManager::GetInstance()->GetMouseY()
@@ -44,9 +52,12 @@ private:
 	bool isHovered = false;
 	AABB* collider = nullptr;
 	OnClick onClick;
+	Text* label;
+	Vector2 labelStartScale;
 
 	void OnHoverEnter() {
 		transform->scale = transform->scale * hoverScaleFactor;
+		label->GetTransform()->scale = label->GetTransform()->scale * hoverScaleFactor;
 		//SetScaleCentered(transform->scale * hoverScaleFactor);
 		AudioManager::GetInstance()->PlaySound("res/audio/sfx/ui_hover.wav");
 
@@ -55,6 +66,7 @@ private:
 
 	void OnHoverExit() {
 		transform->scale = transform->scale / hoverScaleFactor;
+		label->GetTransform()->scale = label->GetTransform()->scale / hoverScaleFactor;
 		isHovered = false;
 	}
 
