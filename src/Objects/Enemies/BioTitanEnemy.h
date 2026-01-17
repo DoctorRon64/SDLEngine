3fbbc6fb683ab2537d4ff368e9bf86a34651a6ad
DrRon <DoctorRon64@users.noreply.github.com>
2026-01-17 03:36:40 +0100

    [FIXED] Bio Titan bigger

--- src/Objects/Enemies/BioTitanEnemy.h
+++ src/Objects/Enemies/BioTitanEnemy.h
@@ -12,6 +12,7 @@ public:
 	BioTitanEnemy(Vector2 spawn)
 		: Enemy(ENEMY_BIO_TITAN_SPRITE_PATH, spawn, { 64, 64 }) {
 		InitHp(200);
+		transform->scale = { 4.f, 4.f };
 	}
 
 	void Update() override {
